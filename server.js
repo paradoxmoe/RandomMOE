3c703b1d00e4c344dda70fbb29d3c6f3d08f2dc5
verysimplyms <verysimplyms@gmail.com>
2019-04-19 11:12:49 -0500

    heroku update

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(function (req, res, next) {
   if(req.secure) {
     next()
   } else {
-    res.redirect("https://" + req.headers.host + ":80" + req.url );
+    res.redirect("https://" + req.headers.host + req.url );
   }
 })
 

7c7624f45676cc5c2fb8762a2e774a46dac82fe1
verysimplyms <verysimplyms@gmail.com>
2019-04-22 13:06:04 -0500

    Contact

--- src/App.js
+++ src/App.js
@@ -142,6 +142,7 @@ class App extends Component {
 
         <GenerateKeys />
         <CanvasBackground />
+        <div id='contact'><a href="https://twitter.com/Twitch_NotDem">Twitter</a> | <a href="https://twitch.tv/notdem">Twitch</a> | <a href="https://github.com/verysimplyms/omreact">Github Repo</a></a></div>
       </div>
     );
   }

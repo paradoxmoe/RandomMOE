890b2e20c86e1aad77204f9653d3e531b77971e0
verysimplyms <verysimplyms@gmail.com>
2019-05-23 15:55:04 -0500

    add build

---
+++ build/precache-manifest.39e25ebe5ddcaa2643ad5d189d05fe89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5b6600311daaf88dca1",
+    "url": "/static/css/main.d6484296.chunk.css"
+  },
+  {
+    "revision": "e5b6600311daaf88dca1",
+    "url": "/static/js/main.5dd03310.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7dc738b616f65aa162a",
+    "url": "/static/js/2.e88609e9.chunk.js"
+  },
+  {
+    "revision": "11bdbe18418faa9188f2e10633b252bd",
+    "url": "/index.html"
+  }
+];

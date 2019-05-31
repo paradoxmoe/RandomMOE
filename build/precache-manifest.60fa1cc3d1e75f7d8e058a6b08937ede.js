533b41a5616ee5ff3e49ba2f3ba0e481f83dc283
verysimplyms <verysimplyms@gmail.com>
2019-05-31 14:28:26 -0500

    Simmer support, site intro updated

---
+++ build/precache-manifest.60fa1cc3d1e75f7d8e058a6b08937ede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4dc565b2246797839bd",
+    "url": "/static/css/main.d6484296.chunk.css"
+  },
+  {
+    "revision": "d4dc565b2246797839bd",
+    "url": "/static/js/main.c109424b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac1e62fab3d8d07fe38b",
+    "url": "/static/js/2.a74bbc6f.chunk.js"
+  },
+  {
+    "revision": "312674f00e52d84c43038bb80f03e44e",
+    "url": "/index.html"
+  }
+];

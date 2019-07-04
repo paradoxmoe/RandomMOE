0202521e664309002c843cf92566a4d99a226957
verysimplyms <verysimplyms@gmail.com>
2019-07-04 12:53:24 -0500

    Added .env file compatibility to separate between dev mode and production mode easier

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbe19ede3107fa713e16998afab76c1b.js"
+  "/precache-manifest.f14a4ff7f345a6ddc2abe7b1ae5b91e0.js"
 );
 
 workbox.clientsClaim();

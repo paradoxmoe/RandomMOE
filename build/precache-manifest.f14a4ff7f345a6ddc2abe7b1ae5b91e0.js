5274ed69d4085a73050f19af84b30acfcaca4a5d
verysimplyms <verysimplyms@gmail.com>
2019-07-04 12:53:28 -0500

    Added .env file compatibility to separate between dev mode and production mode easier

---
+++ build/precache-manifest.f14a4ff7f345a6ddc2abe7b1ae5b91e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f1968a0c6218b4ec7c",
+    "url": "/static/css/main.4cc9979b.chunk.css"
+  },
+  {
+    "revision": "45f1968a0c6218b4ec7c",
+    "url": "/static/js/main.a6c9a435.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac1e62fab3d8d07fe38b",
+    "url": "/static/js/2.a74bbc6f.chunk.js"
+  },
+  {
+    "revision": "66a204d687170907a533397e6b883028",
+    "url": "/index.html"
+  }
+];
